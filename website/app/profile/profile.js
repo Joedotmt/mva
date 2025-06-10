08159e68a2050b9e1d0d58eec0253362831bc202
Joedotmt <99688750+Joedotmt@users.noreply.github.com>
2025-06-11 00:04:16 +0200

    Update profile.js

--- website/app/profile/profile.js
+++ website/app/profile/profile.js
@@ -44,7 +44,7 @@ async function loadProfileData()
 {
     if (!pb.authStore.isValid || !pb.authStore.model || pb.authStore.model.collectionName !== VETERANS_COLLECTION || !loggedInVeteran)
     {
-        console.log("eeee");
+        console.log([!pb.authStore.isValid, !pb.authStore.model, pb.authStore.model.collectionName !== VETERANS_COLLECTION, !loggedInVeteran]);
         //if (pb) pb.authStore.clear(); // Clear potentially invalid store
         //window.location.href = '/mva'; // Redirect to login/home
         //return;
