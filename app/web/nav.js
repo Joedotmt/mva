bc810cd5bf0b592898e508b0c897a20600c2de3d
Joe Grech Esposito <99688750+Joedotmt@users.noreply.github.com>
2026-08-07 02:12:50 +0200

    removed old web recreation

--- app/web/nav.js
+++
@@ -1,9 +0,0 @@
-// JavaScript to dynamically load the navigation bar
-document.addEventListener("DOMContentLoaded", function () {
-    fetch('/portal/web/nav.html')
-        .then(response => response.text())
-        .then(data => {
-            nav_container.insertAdjacentHTML('afterbegin', data);
-        })
-        .catch(error => console.error('Error loading navigation:', error));
-});
